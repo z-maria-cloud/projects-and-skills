29c9be2932b3e5ec2a186b77aa1bf1a956a3c989
name <you@example.com>
2025-08-30 21:38:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My projects and skills
+
+A simple website that showcases my programming projects. Also showcases which programming languages, frameworks and software I use.
---
+++ index.js
@@ -0,0 +1,19 @@
+// My projects and skills
+
+import 'dotenv/config'
+import express from 'express';
+
+const app = express();
+const port = process.env.PROJECT_PORT;
+const appName = process.env.PROJECT_NAME;
+
+app.get('/', (req, res) => {
+  res.render("main.ejs")
+});
+
+app.listen(port, () => {
+  if (process.env.NODE_ENV === "production") {
+    console.log("SMILE! you're in production");
+  }
+  console.log(`${appName} is listening on port ${port}`);
+});
